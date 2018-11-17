87d5b8e69fc000ff22805b979ba18ba7b39e210c
Meoil <meoillabs@gmail.com>
2018-11-16 23:20:44 -0500

    Language that does not work!

--- src/main/java/com/meoil/teststuff/TestStuff.java
+++ src/main/java/com/meoil/teststuff/TestStuff.java
@@ -16,7 +16,7 @@ public class TestStuff {
     public static final String NAME = "Test Stuff";
     public static final String VERSION = "1.0";
 
-    public static CreativeTabs TAB_TESTSTUFF = new CreativeTabs("Test Stuff") {
+    public static CreativeTabs TAB_TESTSTUFF = new CreativeTabs("teststuff") {
 		@Override
 		public ItemStack getTabIconItem() {
 			return new ItemStack(Items.FEATHER);
